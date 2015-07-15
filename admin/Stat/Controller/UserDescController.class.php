59bca690b93bff1e32091e87c18a16b3e13f3c01
bellalu <bellalu@iknowdoctor.com>
2015-07-15 16:48:51 +0800

    用户设备信息统计接口

---
+++ admin/Stat/Controller/UserDescController.class.php
@@ -0,0 +1,47 @@
+<?php
+namespace Stat\Controller;
+use Think\Controller;
+use Stat\Model\DescModel as DescModel;
+
+class UserDescController extends Controller {
+
+	public function _initialize()
+	{
+		$this->desc = D('Desc');
+	}
+
+	## 用户设备信息（可实时）
+	public function deviceSummary()
+	{
+		$re = $this->user->calDevice();
+		$this->writeLog($re, 'Stat\UserDesc\deviceSummary');
+	}
+
+	
+	private function writeLog($result, $tag)
+	{
+		$log_str = "";
+		$time = date('Y-m-d H:i:s', time());
+		$log_str .= $time . ' ' . $tag . ': ' . $result;
+		$log_str .= "\n";
+		$dir_name = dirname(dirname(dirname(__FILE__)));
+		$dir_name = $dir_name . "/Runtime/ScriptLogs/";
+		$date = date('Y-m-d', time());
+		$file_name = $dir_name . $date . ".txt";
+		try
+		{
+			$f_obj = fopen($file_name, 'a');
+			$f_result = fwrite($f_obj, $log_str);
+			fclose($f_obj);
+		}
+		catch(Exception $e)
+		{
+			print $e->getMessage();
+			exit();
+		}
+	}
+
+	
+
+	
+}
---
+++ admin/Stat/Model/DescModel.class.php
@@ -0,0 +1,70 @@
+<?php
+namespace Stat\Model;
+use Think\Model;
+
+class DescModel extends Model {
+    protected $connection = 'DB_IMED';
+    protected $trueTableName = 't_login_flow';
+
+    public function _initialize()
+    {
+        $this->stat_config = C('DB_STAT');
+        // var_dump($stat_config);
+    }
+
+    public function calDevice()
+    {
+        $sql = "SELECT MAX(login_time) login_time FROM t_user_device_flow";
+        $re = queryByNoModel('t_user_device_flow', '', $this->stat_config, $sql);
+        if($re === false)
+            return false;
+        $where_clause = ' 1';
+        if(!is_null($re[0]['login_time']))
+        {
+            $max_time = $re[0]['login_time'];
+            $where_clause = " create_time >'" . $max_time . "'";
+        }
+        $select_sql = <<<EOF
+        SELECT user_uid, device_token, create_time FROM imed.`t_login_flow` 
+        WHERE `status` IN (1, 2) AND {$where_clause}
+EOF;
+        $flow = $this->query($select_sql);
+        if($flow === false)
+            return false;
+        for($i = 0; $i < count($flow); $i++)
+        {
+            $insert_data = array();
+            $insert_data['user_uid'] = $flow[$i]['user_uid'];
+            $insert_data['login_time'] = $flow[$i]['create_time'];
+            $device_ary = explode('_', $flow[$i]['device_token']);
+            if(count($device_ary) != 4)
+                continue;
+            for($k = 0; $k < count($device_ary); $k++)
+            {
+                switch($k)
+                {
+                    case 0:
+                        $insert_data['user_mobile'] = $device_ary[$k];
+                        break;
+                    case 1:
+                        $insert_data['sdk'] = $device_ary[$k];
+                        break;
+                    case 2:
+                        $insert_data['sys_version'] = $device_ary[$k];
+                        break;
+                    case 3:
+                        $insert_data['app_version'] = $device_ary[$k];
+                        break;
+                    default:
+                        break;
+                }
+            }
+            var_dump($insert_data); exit;
+            $result = insertByNoModel('t_user_device_flow', '', $this->stat_config, $insert_data);
+            if($result === false)
+                return false;
+        }
+        return true;
+    }
+
+}
