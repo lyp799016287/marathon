81365bb4223beb42124eaf197520a5873d6454fc
mandy <mandyzhou@iknowdoctor.com>
2015-07-21 10:55:03 +0800

    秘密主题修改-与客户端一致

--- admin/Manage/Conf/config.php
+++ admin/Manage/Conf/config.php
@@ -1,14 +1,14 @@
 <?php
 
 $secret_theme = array(
-	array('id'=>101, 'img'=>'#7e88e9'),
-	array('id'=>102, 'img'=>'#00a3ff'),
-	array('id'=>103, 'img'=>'#30c4ea'),
-	array('id'=>104, 'img'=>'#52de7e'),
-	array('id'=>105, 'img'=>'#ff6379'),
-	array('id'=>106, 'img'=>'#ff6d42'),
-	array('id'=>107, 'img'=>'#feb100'),
-	array('id'=>108, 'img'=>'#4e4e4e')
+	array('id'=>0, 'img'=>'#7e88e9'),
+	array('id'=>1, 'img'=>'#00a3ff'),
+	array('id'=>2, 'img'=>'#30c4ea'),
+	array('id'=>3, 'img'=>'#52de7e'),
+	array('id'=>4, 'img'=>'#ff6379'),
+	array('id'=>5, 'img'=>'#ff6d42'),
+	array('id'=>6, 'img'=>'#feb100'),
+	array('id'=>7, 'img'=>'#4e4e4e')
 ); 
 
 return array(
