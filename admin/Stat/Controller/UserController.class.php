7b2eac863eb8477c5f8f4ca73c0d971266a47271
bellalu <bellalu@iknowdoctor.com>
2015-07-22 14:47:12 +0800

    恢复原文件

--- admin/Stat/Controller/UserController.class.php
+++ admin/Stat/Controller/UserController.class.php
@@ -53,37 +53,5 @@ class UserController extends Controller {
 			exit();
 		}
 	}
-
-	## 累计用户数展示
-	public function cumulationData()
-	{
-		## 展示的时间类型
-		## 1： 天
-		## 2： 周
-		## 3： 月
-		$type = I('type', 1, 'intval');
-		$result = $this->user->getLatestCumu($type); 
-		if(!empty($result))
-		{
-			header("Content-Type: text/html;charset=utf-8");
-			exit("信息获取失败");
-		}
-		else
-		{
-			$this->assign("data", $result);
-			$this->display("cumulateUser");
-		}
-	}
-
-	## 登录用户 活跃用户展示
-	public function activeData()
-	{
-
-	}
-
-	## 留存率展示
-	public function retainData()
-	{
-		
-	}
+	
 }
