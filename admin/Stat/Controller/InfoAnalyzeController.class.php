ff0591fc52ada898b64c48cea7ca0387810dbca2
bellalu <bellalu@iknowdoctor.com>
2015-08-14 15:07:13 +0800

    新增分析资讯关键词模块

---
+++ admin/Stat/Controller/InfoAnalyzeController.class.php
@@ -0,0 +1,53 @@
+<?php
+namespace Stat\Controller;
+use Think\Controller;
+use Stat\Model\InfoAnalyzeModel as InfoAnalyzeModel;
+
+class InfoAnalyzeController extends Controller {
+
+	public function _initialize()
+	{
+		var_dump("into init");
+		$this->ana = D('InfoAnalyze');
+	}
+
+	## 拆分表t_info_daily/t_info_accumulate表中的keys字段
+	public function keySplit()
+	{
+		$result = $this->ana->intoWords();
+		$this->writeLog($result, 'Stat/InfoAnalyze/keySplit');
+	}
+
+	## 跑脚本的内容  写log
+	private function writeLog($result, $tag)
+	{
+		$log_str = "";
+		$time = date('Y-m-d H:i:s', time());
+		$log_str .= $time . ' ' . $tag . ': ' . $result['code'] . "   " . $result['message'];
+		// if($result !== false)
+		// {
+		// 	for($i = 0; $i < count($result); $i++)
+		// 		$log_str .= $result[$i] . ", ";
+		// }
+		// else
+		// 	$log_str .= 'execute error';
+		$log_str .= "\n";
+		$dir_name = dirname(dirname(dirname(__FILE__)));
+		$dir_name = $dir_name . "/Runtime/ScriptLogs/";
+		$date = date('Y-m-d', time());
+		$file_name = $dir_name . $date . ".txt";
+		try
+		{
+			$f_obj = fopen($file_name, 'a');
+			$f_result = fwrite($f_obj, $log_str);
+			fclose($f_obj);
+		}
+		catch(Exception $e)
+		{
+			print $e->getMessage();
+			exit();
+		}
+	}
+
+	
+}
---
+++ admin/Stat/Model/InfoAnalyzeModel.class.php
@@ -0,0 +1,106 @@
+<?php
+namespace Stat\Model;
+use Think\Model;
+
+class InfoAnalyzeModel extends Model {
+	protected $connection = 'DB_STAT';
+    protected $trueTableName = 't_info_daily';
+
+    public function _initialize()
+    {
+        $this->file_name = 't_info_accumulate_time.txt';
+    }
+
+    public function intoWords()
+    {
+    	// 读取文件 获得当前最大的时间戳
+    	$time_str = '';
+    	try
+    	{
+    		$f_obj = fopen($this->file_name, 'r');
+    		if($f_obj)
+    		{
+    			$time_str = fgets($f_obj);
+    			$time_str = trim($time_str);
+    		}
+    		fclose($f_obj);
+    	}
+    	catch(Exception $e)
+		{
+			print $e->getMessage();
+			// exit();
+			return array('code'=>-4, 'message'=>'读取文件失败');
+		}
+    	
+		var_dump($time_str);
+    	$where_clause = '';
+    	if($time_str != '')
+	    	$where_clause = " WHERE create_time > '" . $time_str . "'";
+	    $sql = "SELECT info_id, `keys` FROM t_info_accumulate " . $where_clause;
+	    $result = $this->query($sql);
+	    var_dump($result);
+	    if($result === false)
+	    	return array('code'=>-1, 'message'=>'查询错误');
+	    $re = $this->splitWords($result);
+	    if($re === true)
+	    {
+	    	//获取最大的时间戳 重新写入
+	    	$sql = "SELECT MAX(create_time) create_time FROM t_info_accumulate " . $where_clause;
+	    	$re_time = $this->query($sql);
+	    	if($re_time === false)
+	    		return array('code'=>-3, 'message'=>'获取时间戳失败');
+	    	$time_str = $re_time[0]['create_time'];
+	    	$re_file = $this->intoFile($time_str);
+	    	if($re_file === false)
+	    		return array('code'=>-5, 'message'=>'写入文件失败');
+	    	return array('code'=>1, 'message'=>'执行成功');
+	    }
+	    else
+	    	return array('code'=>-2, 'message'=>'插入数据错误');
+    }
+
+	private functiong splitWords($ary)
+	{
+		$error_str = "";
+		for($i = 0; $i < count($ary); $i++)
+		{
+			$info_id = $ary[$i]['info_id'];
+			$words = explode(',', $ary[$i]['keys']);
+			for($j = 0; $j < count($words); $j++)
+			{
+				$word = $words[$j];
+				$insertSql = <<<EOF
+				INSERT INTO t_info_keys (info_id, key_word)
+				VALUES({$info_id}, "{$word}")
+EOF;
+				$re = $this->execute($insertSql);
+				if($re === false)
+					$error_str .= $info_id . ": " . $word . ", ";
+			}
+		}
+		if($error_str == '')
+			return true;
+		else
+			return $error_str;
+	}
+
+	## 最大时间戳写入文件
+	private function intoFile($str)
+	{
+		try
+		{
+			$f_obj = fopen($this->file_name, 'w');
+			fwrite($f_obj, $str);
+			fclose($f_obj);
+		}
+		catch(Exception $e)
+		{
+			print $e->getMessage();
+			// exit();
+			return false;
+		}
+		return true;
+	}
+
+
+}
