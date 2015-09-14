0272f47324f313016d87397195f999375d8a2b6f
mandy <mandyzhou@iknowdoctor.com>
2015-09-14 10:30:34 +0800

    debug

--- admin/Home/Controller/IndexController.class.php
+++ admin/Home/Controller/IndexController.class.php
@@ -25,4 +25,30 @@ class IndexController extends Controller {
         $this->display("index");
         //print_r($menuOrder);//exit();
     }
+
+	/*public function download(){
+		import('Vendor.StatExcel.StatExcel');
+		$excel = new \StatExcel();
+		//var_dump($excel);
+
+		$columns = array(
+			array('id' => 's_date', 'name' => '时间', 'isSort' => true, 'issortshow' => true, 'width' => 90),
+			array('id' => 'module_name', 'name' => '页面模块', 'width' => 120, 'isSort' => true, 'issortshow' => true),
+			array('id' => 'click_num', 'name' => '点击量', 'width' => 120, 'isSort' => true, 'issortshow' => true, 'type' => 'int', 'align' => 'right'),
+			array('id' => 'order_num', 'name' => '下单笔数', 'width' => 120, 'isSort' => true, 'issortshow' => true, 'type' => 'int', 'align' => 'right'),
+			array('id' => 'order_fee', 'name' => '下单金额', 'width' => 120, 'isSort' => true, 'issortshow' => true, 'type' => 'int', 'align' => 'right'),
+			array('id' => 'trans_rate', 'name' => '转化率', 'width' => 120, 'isSort' => true, 'issortshow' => true, 'align' => 'right')
+		);
+
+		$report_title = "无线实时统计报表";
+
+		$excel->addParams(
+			array(
+				'title' => $report_title,
+				'header' => array($columns),
+				'data' => array()	//数据集
+			)
+		);		
+		$excel->exportAsXsl($report_title);
+	}*/
 }
