9129f35e2e9ca724921165bf63d7193d8f774bcf
mandy <mandyzhou@iknowdoctor.com>
2015-09-29 11:17:09 +0800

    用户管理模块更新

--- admin/Admin/Controller/UserController.class.php
+++ admin/Admin/Controller/UserController.class.php
@@ -14,7 +14,26 @@ class UserController extends Controller {
 		$this->assign("menu_path", ROOT_PATH.'/admin_imed_me/');
 	}
 
-	public function show(){
+	public function userList(){
+		$where = '1';
+		
+		$sql = "SELECT * FROM t_user WHERE STATUS != -1 ORDER BY create_time DESC";
+		$ret = queryByNoModel('t_user', '', 'DB_ADMIN', $sql);
+		
+		$curr_page = I('page', 1, 'intval');
+		$page_size = 20;
+
+		if(!empty($ret)){
+			$limit = ($curr_page - 1)*$page_size;
+			$data = array_slice($ret, $limit, $page_size);
+		}
+		
+		$total = count($ret);
+		$total_num = ceil($total/$page_size);
+		$this->assign("list", $data);
+		$this->assign("total", $total);
+		$this->assign("current", $curr_page);
+		$this->assign("total_num", $total_num);
 		$this->display("UserManage");
 	}
 	
@@ -27,5 +46,33 @@ class UserController extends Controller {
 			$this->ajaxReturn(1, 'JSON');
 		}
 		$this->ajaxReturn($ret, 'JSON');
+	}
+
+	public function userDel(){
+		
+		$id = I('id', 0, 'intval');
+
+		$sql = "UPDATE t_user SET status = -1, modify_time = NOW() WHERE id = ".$id;
+		$rs = updateByNoModel('t_user', '', 'DB_ADMIN', $sql);
+
+		if($rs === false){
+			$this->ajaxReturn(array('code'=>-1, 'message'=>'删除失败'), 'JSON');
+		}else{
+			$this->ajaxReturn(array('code'=>1, 'message'=>'删除成功'), 'JSON');
+		}
+	}
+
+	public function userChk(){
+		
+		$id = I('id', 0, 'intval');
+		
+		$sql = "UPDATE t_user SET status = 2, modify_time = NOW() WHERE id = ".$id;
+		$rs = updateByNoModel('t_user', '', 'DB_ADMIN', $sql);
+
+		if($rs === false){
+			$this->ajaxReturn(array('code'=>-1, 'message'=>'审核失败'), 'JSON');
+		}else{
+			$this->ajaxReturn(array('code'=>1, 'message'=>'审核成功'), 'JSON');
+		}
 	}
 }
